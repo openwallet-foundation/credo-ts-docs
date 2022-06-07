fb89af038fe354dcbed84ae95a9b7e07e727cb8f
Karim Stekelenburg <karim@animo.id>
2022-06-07 13:27:26 +0200

    feat: add snippet import with section comments (#19)
    * feat: add snippet import with section comments
    
    Signed-off-by: Karim Stekelenburg <karim@animo.id>
    
    * Update src/theme/CodeBlock/Line/index.js
    
    Co-authored-by: Timo Glastra <timo@animo.id>
    
    * fix: rendering of normal snippets
    
    Signed-off-by: Karim Stekelenburg <karim@animo.id>
    
    Co-authored-by: Timo Glastra <timo@animo.id>

---
+++ src/theme/CodeBlock/Line/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Line from "@theme-original/CodeBlock/Line";
+
+export default function LineWrapper(props) {
+
+  return (
+    <>
+      <Line {...props} />
+    </>
+  );
+}
