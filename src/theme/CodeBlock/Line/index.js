2153965903402ebc1aa960a45ef2a8af1ae5d03f
Karim Stekelenburg <karim@animo.id>
2022-06-07 13:16:06 +0200

    Update src/theme/CodeBlock/Line/index.js
    Co-authored-by: Timo Glastra <timo@animo.id>

--- src/theme/CodeBlock/Line/index.js
+++ src/theme/CodeBlock/Line/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Line from "@theme-original/CodeBlock/Line";
 
 export default function LineWrapper(props) {
-  // console.log('From LineWrapper, props:', props)
 
   return (
     <>
